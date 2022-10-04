4c2c21cf58b676c216b637255f4e633e5973da39
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-10-04 23:21:59 +0530

    Updated file

---
+++ Models/car.models.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose')
+
+const carSchema = new mongoose.Schema({
+    registerNumber: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    vehicleNumber: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    body: {
+        type: String,
+        required: true
+    }
+
+
+})
+
+module.exports = mongoose.model('Car', carSchema)
