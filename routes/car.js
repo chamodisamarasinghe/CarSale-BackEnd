ad5310ae41fe1f9f0d14a6152a483502290a5fd7
chamodisamarasinghe <chamodisandunika98@gmail.com>
2022-10-05 20:52:48 +0530

    Updated

--- routes/car.js
+++ routes/car.js
@@ -1,75 +1,50 @@
-// const express = require('express')
-// const app = express()
-// const router = express.Router()
+const express = require('express');
+const router = express.Router();
+const mysql = require('mysql');
+const db = require('../configs/db.configs');
+const { query } = require('express');
 
-// app.use(express.json())
+const connection = mysql.createConnection(db.database);
 
-// const Car = require('../Models/car.models')
+connection.connect(function (err) {
+    if (err) {
+        console.log(err);
+    } else {
+        console.log('connected to the MySQL Server');
+        var userTableQuery = "CREATE TABLE IF NOT EXISTS cars (registerNumber VARCHAR(255) PRIMARY KEY, brand VARCHAR(255), vehicleNumber VARCHAR(255), price VARCHAR(255))";
+        connection.query(userTableQuery, function (err, result) {
+            if (result.warningCount === 0) {
+                console.log("User table created");
+            }
+        })
+    }
+})
 
-// router.get('/', async(req,res)=>{
-//     try {
-//         const cars = await Car.find()
-//         res.send(cars)
-//         console.log("car get");
-//     } catch (error) {
-//         res.send(error)
-//     }
-// })
 
-// router.post('/', (req,res)=>{
-//     const data = req.body.data
-//     console.log(data);
-//     const newCar = new Car({
-//         registerNumber: data.registerNumber,
-//         brand: data.brand,
-//         vehicleNumber: data.vehicleNumber,
-//         price: data.price
-        
-//     });
-//     try {
-//         const response = newCar.save()
-//         res.send("Saved!")
-//     } catch (error) {
-//         res.send(error)
-//     }
-// })
+router.post('/', (req, res) => {
+    const registerNumber = req.body.registerNumber;
+    const brand = req.body.brand;
+    const vehicleNumber = req.body.vehicleNumber;
+    const price = req.body.price;
+    
 
-// router.put('/:id', async(req,res)=>{
-//     const data = req.body
+    var query = "INSERT INTO users (registerNumber,brand,vehicleNumber,price) VALUES (?,?,?,?)";
 
-//     try {
-//         const car = await Car.findById(req.params.id)
-//         car.registerNumber = data.registerNumber,
-//         car.brand = data.brand,
-//         car.vehicleNumber = data.vehicleNumber,
-//         car.price = data.price
-       
-        
-//         const response = await car.save()
+    connection.query(query, [registerNumber, brand, vehicleNumber, price], (err) => {
+        if (err) {
+            res.send({
+                "status": "500",
+                "message": "RegisterNum Already Exists!"
+            });
+        } else {
+            res.send({
+                "status": "200",
+                "message": "Car saved successfully"
+            });
+        }
+    })
 
-//         res.send("Updated!")
-//     } catch (error) {
-//         res.send(error)
-//     }
-// })
+});
 
-// router.delete('/:id', async(req,res)=>{
-//     try {
-//         const car = await Car.findById(req.params.id)
-//         const response = car.remove()
-//         res.send("Deleted!")
-//     } catch (error) {
-//         res.send(error)
-//     }
-// })
 
-// router.get('/:id', async(req,res)=>{
-//     try {
-//         const car = await Car.findById(req.params.id)
-//         res.send(car)
-//     } catch (error) {
-//         res.send(error)
-//     }
-// })
-
-// module.exports = router
+module.exports = router
